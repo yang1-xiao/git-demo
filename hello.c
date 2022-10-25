6eb73835427523fa7af6a259601b70db44b9e4d3
Xiao <2771005430@qq.com>
2022-10-25 15:54:14 +0800

    second cmmit

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])sdcadnkcnadclanc
 {
 	printf("Hello world\n");
 	return 0;
 }
+cnksdcbjadjcalcnalka
+canclamcama;dama
